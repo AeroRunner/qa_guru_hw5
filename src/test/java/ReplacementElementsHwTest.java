2e855795d5fe05cab1c8d52312417b80b3433967
Dmitrii Elizarov <airplay1x6@gmail.com>
2023-11-01 14:18:41 +0300

    done2!

--- src/test/java/ReplacementElementsHwTest.java
+++ src/test/java/ReplacementElementsHwTest.java
@@ -1,28 +1,28 @@
 import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ReplacementElementsHwTest {
+
     @BeforeAll
-    static void beforeAll() {
+   static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.timeout = 5000;
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl ="https://the-internet.herokuapp.com";
-    }
+                      }
     @Test
     void AactionB() {
         open("/drag_and_drop");
         $("#column-a").shouldHave(text("A"));
         $("#column-b").shouldHave(text("B"));
-        actions().dragAndDropBy($("#column-a"), 200, 0).build().perform();
+        actions().moveToElement($("#column-a")).clickAndHold().moveByOffset(200,0).release().perform();
         $("#column-a").shouldHave(text("B"));
         $("#column-b").shouldHave(text("A"));
-                    }
 
+                    }
     @Test
     void AdragAndDroptoB() {
         open("/drag_and_drop");
@@ -32,4 +32,5 @@ public class ReplacementElementsHwTest {
         $("#column-a header").shouldHave(text("B"));
         $("#column-b header").shouldHave(text("A"));
                            }
+
 }
